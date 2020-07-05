62073c1462c1e634458ad6c2524c8d3401f85959
A Naveen <66691255+anaveen200811@users.noreply.github.com>
2020-07-05 19:21:32 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,29 @@
+class Bob {
+    constructor(x,y,radius) {
+        var options = {
+            isStatic:false,
+            restitution:1,
+            friction:0,
+            density:0.8
+        };
+        this.x=x;
+        this.y=y;
+        this.radius = radius;
+        this.body = Matter.Bodies.circle(this.x, this.y, (this.radius)/2,options);
+        World.add(world, this.body);
+        
+    };
+
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill("green");
+        ellipse(0,0, this.radius);
+        pop();
+    }
+      
+};
+
